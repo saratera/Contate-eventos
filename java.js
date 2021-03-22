8583133385cedb585457d80e63b9cc87c552921c
Valdeir Sarat <80068540+saratera@users.noreply.github.com>
2021-03-22 16:04:32 -0300

    Add files via upload
    http://contateeventos.com.br/
    Página está no ar.
    Falta fazer alguns ajustes de responsabilidade e no JavaScript.

---
+++ java.js
@@ -0,0 +1,17 @@
+
+ 
+document.querySelector(".menu-mobi").addEventListener("click", ()=>{
+    var b = document.getElementById("conteudo-menu-int");
+    if (b.style.display == "block"){
+        b.style.display = "none";
+
+    }else{
+            b.style.display = "block"
+        }
+    
+});
+    
+
+
+
+
